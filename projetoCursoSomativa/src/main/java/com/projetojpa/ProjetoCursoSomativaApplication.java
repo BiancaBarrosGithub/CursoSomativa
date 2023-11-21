fe606406bc8a2da9cb5fe670b63f7375c71ef667
BiancaBarrosGithub <146129798+BiancaBarrosGithub@users.noreply.github.com>
2023-11-21 10:29:46 -0300

    Add files via upload

---
+++ projetoCursoSomativa/src/main/java/com/projetojpa/ProjetoCursoSomativaApplication.java
@@ -0,0 +1,13 @@
+package com.projetojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCursoSomativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCursoSomativaApplication.class, args);
+	}
+
+}
